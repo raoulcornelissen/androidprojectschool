92a94f33d0b70363a33c6e49d69059f286f0d089
Raoul Cornelissen <raoul@raoulcornelissen.com>
2016-10-10 17:56:40 +0200

    Wat kleine aanpassingen in de code gedaan, en wat uitlijning veranderd op de GetKentekenActivity

--- app/src/main/java/com/congreats/stoppolitie/AboutActivity.java
+++ app/src/main/java/com/congreats/stoppolitie/AboutActivity.java
@@ -3,16 +3,19 @@ package com.congreats.stoppolitie;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by raoul on 10-10-2016.
  */
 
 public class AboutActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+
     }
 
 
